9e6dbfae0ac10655de825d202f54c96a5d997c61
Lolloc99 <lolloc99@yahoo.it>
2022-05-04 14:22:27 +0200

    Snack 3 completato

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -1,3 +1,48 @@
 /*
 3. Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
 */
+
+const squadre = [
+    {
+        nome : "Tediore",
+        puntiSegnati : 0,
+        falliSubiti : 0
+    },
+
+    {
+        nome : "Hyperion",
+        puntiSegnati : 0,
+        falliSubiti : 0
+    },
+
+    {
+        nome : "Jakobs",
+        puntiSegnati : 0,
+        falliSubiti : 0
+    },
+
+    {
+        nome : "Maliwan",
+        puntiSegnati : 0,
+        falliSubiti : 0
+    },
+];
+
+squadre.forEach(element => {
+    element.falliSubiti = getRndInteger(1, 100) 
+    element.puntiSegnati = getRndInteger(1, 100) 
+    console.log("La squadra:", element.nome, "ha subito:", element.falliSubiti, "falli, totalizzando ", element.puntiSegnati, " punti");
+});
+
+console.log(squadre);
+
+// FUNCTIONS
+/*
+Descrizione: Genera numeri casuali
+    Dato 1: range numero minimo → "min"
+    Dato 2: range numero massimo → "max"
+    Return: numero casuale compreso tra "min" e "max"
+*/
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
