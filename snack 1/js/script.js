5b878a8a539ec539977830b868d0f3a1aba69848
Lolloc99 <lolloc99@yahoo.it>
2022-05-04 13:56:03 +0200

    Snack 1 completato

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -1,3 +1,12 @@
 /*
 1. Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 */
+
+const array1 = [1, 1, 1, 1];
+const array2 = [1, 1, 1, 1, 1, 1, 1, 1];
+const element = 1;
+
+while (array1.length < array2.length) {
+    array1.push(element)
+    console.log(array1, array2);
+};
