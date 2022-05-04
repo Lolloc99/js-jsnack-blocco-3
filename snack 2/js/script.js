cb86c8280fdbed53199f467a590deca4c66f603b
Lolloc99 <lolloc99@yahoo.it>
2022-05-04 14:06:42 +0200

    Snack 2 completato

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -1,3 +1,21 @@
 /*
-2. Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando a somma degli elementi è minore di 50.
+2. Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
 */
+
+const array = [];
+let sum = 0;
+
+while (sum < 50) {
+    let num = parseInt(prompt("Inserisci un numero"));
+
+    if ((sum + num) < 50) {
+        array.push(num)
+        sum += num
+    } else if ((sum + num) >= 50) {
+        break
+    }
+
+    console.log(array, sum);
+};
+
+console.log(array, sum);
